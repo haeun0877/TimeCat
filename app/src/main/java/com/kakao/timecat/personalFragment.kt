a0426a0e46562d8c89d3afe269db2b79c6baea6b
haeun0877 <70643208+haeun0877@users.noreply.github.com>
2021-04-16 19:32:41 +0900

    profile update

--- app/src/main/java/com/kakao/timecat/personalFragment.kt
+++ app/src/main/java/com/kakao/timecat/personalFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.NotificationManagerCompat.from
+import com.bumptech.glide.Glide
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.android.synthetic.main.fragment_personal.*
 
 class personalFragment : Fragment() {
     override fun onCreateView(
@@ -12,7 +17,15 @@ class personalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        UserApiClient.instance.me { user, error ->
+            if(user?.kakaoAccount?.profile?.profileImageUrl!=null){
+                Glide.with(this).load(user?.kakaoAccount?.profile?.profileImageUrl).into(imageView)
+            }else{
+                Glide.with(this).load(R.drawable.cat3).into(imageView)
+            }
 
+            nickname.text = user?.kakaoAccount?.profile?.nickname +"님"
+        }
 
         return inflater.inflate(R.layout.fragment_personal, container, false)
     }
